b0b9b0c02932aa4ae7691036405042ea8f115d59
Marc Päpper <m.paepper@lemundo.de>
2016-01-19 17:40:23 +0100

    Added function to get random fortune cookie, but not sure if this is correct.

--- client/app.js
+++ client/app.js
@@ -2,6 +2,11 @@ Template.body.helpers({
   
   fortuneCookies: function() {
     return FortuneCookies.find();
+  },
+  
+  getRandomFortuneCookie: function() {
+      var allFortuneCookies = FortuneCookies.find().fetch();
+      return Random.choice(allFortuneCookies);
   }
   
 });
