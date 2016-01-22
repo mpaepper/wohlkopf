26b9db1975fcac383d6c182f383d1c250cbcc05c
Marc Paepper <m.paepper@lemundo.de>
2016-01-22 08:02:45 +0100

    Put fortuneCookie in session, so we can deliver on click. The alternative would be to already fill the random cookie and then just hide / show the div. This version has the advantage that we could also allow to show more than one cookie, e.g. 3 or unlimited.

--- client/app.js
+++ client/app.js
@@ -1,12 +1,18 @@
 Template.body.helpers({
-  
-  fortuneCookies: function() {
-    return FortuneCookies.find();
-  },
-  
-  getRandomFortuneCookie: function() {
-      var allFortuneCookies = FortuneCookies.find().fetch();
-      return Random.choice(allFortuneCookies);
-  }
-  
+    fortuneCookies: function () {
+        return FortuneCookies.find();
+    },
+    getRandomFortuneCookie: function () {
+        return Session.get('fortuneCookie');
+    }
+});
+
+Template.body.events({
+    'click #fortuneCookieButton': function () {
+        if (!Session.get('fortuneCookie')) {
+            var allFortuneCookies = FortuneCookies.find().fetch();
+            var cookie = Random.choice(allFortuneCookies);
+            Session.set('fortuneCookie', cookie);
+        }
+    }
 });
