78278d85b17196c5fc05fea2cef7b50ccc22c2f1
Marc Paepper <m.paepper@lemundo.de>
2016-02-02 09:00:27 +0100

    Added schema for fortune cookies

--- collections/fortuneCookies.js
+++ collections/fortuneCookies.js
@@ -1,1 +1,17 @@
 FortuneCookies = new Mongo.Collection('fortuneCookies');
+
+var Schemas = {};
+
+Schemas.FortuneCookies = new SimpleSchema({
+    wisdom: {
+        type: String,
+        label: "Text",
+        max: 512
+    },
+    author: {
+        type: String,
+        label: "Author"
+    }
+});
+
+FortuneCookies.attachSchema(Schemas.FortuneCookies);
