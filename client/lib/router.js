9afa80c3447d075074d97a799f9d6879a01ac6d5
Marc Paepper <m.paepper@lemundo.de>
2016-02-14 10:35:49 +0100

    Implemented basic page routing

--- client/lib/router.js
+++ client/lib/router.js
@@ -16,3 +16,16 @@ Router.route('insertCookie', function () {
 }, {
     name: 'insertCookie'
 });
+Router.route('/seiten/:topic', function () {
+  this.render(this.params.topic);
+}, {
+    name: 'pages'
+});
+Router.route('/seiten/:topic/:subtopic', function () {
+    var topic = this.params.topic;
+    var subtopic = this.params.subtopic;
+    var template = topic.concat("-").concat(subtopic);
+    this.render(template);
+}, {
+    name: 'subpages'
+});
